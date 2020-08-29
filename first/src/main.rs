24ed579d2f80a6a43b56cc5221d5f6c482fdcaa2
pechuga22 <iefaz.santiagovalle@gmail.com>
2020-08-29 18:34:20 -0500

    add simple function

--- first/src/main.rs
+++ first/src/main.rs
@@ -3,18 +3,29 @@ fn main() {
     println!("this is interested");
 
     let number = 22;
-    println!("THis is the number {}", number);
+    println!("This is the number {}", number);
 
-    let _li = (500, 6.4, 1);
+    let li = (500, 6.4, 1);
 
-    let (_w, _y, _z) = _li;
+    let (_w, y, _z) = li;
 
-    println!("Values {}", _y);
+    println!("Values {}", y);
 
     let a = [1, 2, 3, 4, 5];
-    let index = 3;
+    let _index = 3;
 
-    let element = a[index];
+    // let element = a[index];
 
     println!("The value of element is: {}", a[1]);
+    function(6,6)
+
+}
+
+// Simple function with parameters
+fn function(number_one: i32, number_two: i32){
+    if number_one == number_two{
+        println!("Yes")
+    } else {
+        println!("No")
+    }
 }
