28d351e929eebdb68adb58265acd26a386ab772c
pechuga22 <iefaz.santiagovalle@gmail.com>
2020-08-29 18:19:19 -0500

    add new things

--- first/src/main.rs
+++ first/src/main.rs
@@ -4,4 +4,17 @@ fn main() {
 
     let number = 22;
     println!("THis is the number {}", number);
+
+    let _li = (500, 6.4, 1);
+
+    let (_w, _y, _z) = _li;
+
+    println!("Values {}", _y);
+
+    let a = [1, 2, 3, 4, 5];
+    let index = 3;
+
+    let element = a[index];
+
+    println!("The value of element is: {}", a[1]);
 }
